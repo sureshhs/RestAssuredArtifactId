8f1da9624df3439b544effa349bdaca713e7c0d0
sureshhs <47510939+sureshhs@users.noreply.github.com>
2020-03-07 13:01:41 -0800

    Updated testNG suite and also added cucumber files

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,14 @@
+/*
+package runner;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.*;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "Features/GetUser", glue = "")
+
+public class TestRunner {
+
+}
+*/
